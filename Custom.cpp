143155c64cb55c3fe5316a9e59d92c07e1e77522
Ajay Maheshwari <96301800+AjayMaheshwari23@users.noreply.github.com>
2024-10-11 08:01:26 +0530

    Update Custom.cpp

--- Custom.cpp
+++ Custom.cpp
@@ -23,3 +23,52 @@ struct cmp
 
 
 priority_queue< string  , vector<string> , cmp > pq;
+
+
+/*-------------------- SOMETHING STRANGE WITH SET --------------------------------*/
+
+// For : 
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pii pair<int,int>
+
+auto Compare = [](const pii &lhs, const pii &rhs) {
+    if( lhs.first == rhs.first ) return lhs.second < rhs.second;
+    return lhs.first < rhs.first;
+};
+
+int main()
+{
+    set< pair<int,int> , decltype(Compare) > test;
+    test.insert({1,100});
+    test.insert({1,200});
+    cout << test.size() << "\n";
+}
+
+// Output : 2 
+
+
+/* ---- BUT ---- */
+
+
+// For : 
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pii pair<int,int>
+
+auto Compare = [](const pii &lhs, const pii &rhs) {
+    // if( lhs.first == rhs.first ) return lhs.second < rhs.second; /* ===== Commented OUT ===== */
+    return lhs.first < rhs.first;
+};
+
+int main()
+{
+    set< pair<int,int> , decltype(Compare) > test;
+    test.insert({1,100});
+    test.insert({1,200});
+    cout << test.size() << "\n";
+}
+
+// Output : 1 
